706b9956161b71bd2ff26550a1917ab767dbbd89
alex <xfanth@gmail.com>
2023-11-20 11:54:51 +0000

    IJsonReader has no getFloat method - so don't use it

--- src/base/net/stratum/Pools.cpp
+++ src/base/net/stratum/Pools.cpp
@@ -158,7 +158,7 @@ void xmrig::Pools::load(const IJsonReader &reader)
         }
     }
 
-    setDonateLevel(reader.getFloat(kDonateLevel, kDefaultDonateLevel));
+    setDonateLevel(reader.getInt(kDonateLevel, kDefaultDonateLevel));
     setProxyDonate(reader.getInt(kDonateOverProxy, PROXY_DONATE_AUTO));
     setRetries(reader.getInt(kRetries));
     setRetryPause(reader.getInt(kRetryPause));
